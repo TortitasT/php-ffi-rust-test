d272e84de8918d9561291c4c587e50fa6b80510c
Victor Garcia Fernandez <victorgf2011@gmail.com>
2023-05-11 19:12:37 +0200

    cleanup :)

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-
-// $ffi = FFI::cdef('int add(int a, int b);', __DIR__ . '\target\debug\php_ffi.dll');
-
-$ffi_definition = 'void png_to_avif(char *input, int input_len);';
-$ffi = FFI::cdef($ffi_definition, __DIR__ . '\target\debug\php_ffi.dll');
-
 $string = __DIR__ . '\test.png';
+png_to_avif($string);
 
-$ffi->png_to_avif($string, strlen($string));
+function png_to_avif($input) {
+    $ffi_definition = 'void png_to_avif(char *input, int input_len);';
+    $ffi_path = __DIR__ . '\target\debug\php_ffi.dll';
 
+    $ffi = FFI::cdef($ffi_definition, $ffi_path);
+
+    $ffi->png_to_avif($input, strlen($input));
+}
 ?>
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
-use std::{
-    ffi::CString,
-    fs::File,
-    io::{BufWriter, Write},
-};
-
 use ravif::RGBA8;
+use std::{fs::File, io::Write};
 
 #[no_mangle]
 pub extern "C" fn add(a: i32, b: i32) -> i32 {
@@ -17,29 +12,15 @@ pub extern "C" fn png_to_avif(input: *const libc::c_uchar, input_len: u32) {
         std::str::from_utf8_unchecked(std::slice::from_raw_parts(input, input_len as usize))
     };
 
-    // write path to file
-    let mut file = File::create("./testt.txt").unwrap();
-
-    // write to file
-
     let decoder = png::Decoder::new(File::open(path_str).unwrap());
 
     let mut reader = decoder.read_info().unwrap();
-    // Allocate the output buffer.
     let mut buf = vec![0; reader.output_buffer_size()];
-    // Read the next frame. An APNG might contain multiple frames.
     let info = reader.next_frame(&mut buf).unwrap();
-    // Grab the bytes of the image.
     let bytes = &buf[..info.buffer_size()];
-    // Inspect more details of the last read frame.
-    let in_animation = reader.info().frame_control.is_some();
 
     let width = info.width;
     let height = info.height;
-
-    let infos = format!("{} {} {}", width, height, in_animation);
-
-    file.write_all(infos.as_bytes()).unwrap();
 
     let pixels: &[RGBA8] = unsafe {
         std::slice::from_raw_parts(
@@ -47,7 +28,6 @@ pub extern "C" fn png_to_avif(input: *const libc::c_uchar, input_len: u32) {
             width as usize * height as usize,
         )
     };
-
     let width_usize = width as usize;
     let height_usize = height as usize;
 
@@ -56,10 +36,11 @@ pub extern "C" fn png_to_avif(input: *const libc::c_uchar, input_len: u32) {
         .with_speed(4)
         .encode_rgba(ravif::Img::new(pixels, width_usize, height_usize));
 
-    let mut file = File::create("./test.avif").unwrap();
+    let output_path = path_str.replace(".png", ".avif");
 
+    let mut file = File::create(output_path).unwrap();
     if let Err(e) = encoder {
-        println!("Error: {}", e);
+        println!("Error: {}", e); // This wont print anything i think
         return;
     }
 
